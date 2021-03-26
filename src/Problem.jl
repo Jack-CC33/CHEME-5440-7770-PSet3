40d7cdfcfab6d8a7dd929734c78255ee394d93c7
Jack-CC33 <79158093+Jack-CC33@users.noreply.github.com>
2021-03-25 23:49:48 -0400

    Final changes by deadline

--- src/Problem.jl
+++ src/Problem.jl
@@ -42,11 +42,11 @@ function generate_problem_dictionary()
 	# Setup the flux bounds array -
 	flux_bounds_array = zeros(number_of_reactions,2)
 	# TODO: update the flux_bounds_array for each reaction in your network
-	flux_bounds_array[1,:] = [0, 3.1716]		# [mmol/gDW*hr] Bounds for v1 flux
-	flux_bounds_array[2,:] = [0, 7.308]			# [mmol/gDW*hr] Bounds for v2 flux
-	flux_bounds_array[3,:] = [0, 1.242]			# [mmol/gDW*hr] Bounds for v3 flux
-	flux_bounds_array[4,:] = [0, 8.964]			# [mmol/gDW*hr] Bounds for v4 flux
-	flux_bounds_array[5,:] = [-0.4932, 0.4932]	# [mmol/gDW*hr] Bounds for v5 flux
+	flux_bounds_array[1,:] = [0, 3.1716]		# [mmol/gDW*hr] Bounds for v1 flux  E.C. 2.1.3.3
+	flux_bounds_array[2,:] = [0, 7.308]			# [mmol/gDW*hr] Bounds for v2 flux  E.C. 6.3.4.5
+	flux_bounds_array[3,:] = [0, 1.242]			# [mmol/gDW*hr] Bounds for v3 flux  E.C. 4.3.2.1
+	flux_bounds_array[4,:] = [0, 8.964]			# [mmol/gDW*hr] Bounds for v4 flux  E.C. 3.5.3.1
+	flux_bounds_array[5,:] = [-0.4932, 0.4932]	# [mmol/gDW*hr] Bounds for v5 flux  E.C. 1.14.13.39
 	flux_bounds_array[6:19, 1] .= 0    			# [mmol/gDW*hr]  lower bound on exchange fluxes
 	flux_bounds_array[6:19, 2] .= 10   			# [mmol/gDW*hr]  upper bound on exchange fluxes
 
@@ -54,6 +54,7 @@ function generate_problem_dictionary()
 	species_bounds_array = zeros(number_of_species,2)
 	# TODO: NOTE - we by default assume Sv = 0, so species_bounds_array should be a M x 2 array of zeros
 	# TODO: however, if you formulate the problem differently you *may* need to change this 
+	#    Is this why I'm not getting a result?  I kind of doubt it.
 
 	# Min/Max flag - default is minimum -
 	is_minimum_flag = true
@@ -62,8 +63,7 @@ function generate_problem_dictionary()
 	objective_coefficient_array = zeros(number_of_reactions)
 	# TODO: update me to maximize Urea production (Urea leaving the virtual box) 
 	# TODO: if is_minimum_flag = true => put a -1 in the index for Urea export
-	objective_coefficient_array[17] = -1
-	print(objective_coefficient_array)
+	objective_coefficient_array[17] = -1  # Urea Export:  b12 at rxn index 17 (see Network_Details.dat)
 	
 	
 	# =============================== DO NOT EDIT BELOW THIS LINE ============================== #
